6f8536368a77bb21e1cad52fef33406509d4013f
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-12-07 12:39:15 +0600

    api to get single product data created

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/products/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: id}
+            const result = await productsCollection.findOne(query)
+            res.send(result)
+        })
+
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
